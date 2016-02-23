063d020aae37c7c07b97de5347accaba25b2d106
Jamen Marzonie <jamenmarz@gmail.com>
2016-02-23 12:06:51 -0700

    Fix file extensions unchanged

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 import through from 'through2';
 import pug from 'pug';
+import { join, basename } from 'path';
 import { PluginError } from 'gulp-util';
 
 export default function gulpPug(opts = {}) {
   return through.obj(function stream(original, enc, callback) {
-    const file = original.clone();
     let error = null;
     let contents = null;
 
     if (file.isBuffer()) {
-      contents = pug.compile(file.contents.toString(), opts)(opts.locals);
+      contents = pug.compile(original.contents.toString(), opts)(opts.locals);
     } else {
       error = new PluginError('gulp-pug', 'Unsupported file content');
     }
 
-    file.contents = new Buffer(contents);
+    const file = original.clone({
+      contents: new Buffer(contents),
+      extname: '.html',
+    });
     callback(error, file);
   });
 };
